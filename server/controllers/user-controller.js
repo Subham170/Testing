668ca1d1047283c5b8aded903436ce867fee3717
Subham Dey <deysubham3312@gmail.com>
2024-05-11 02:25:52 +0530

    2nd

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -180,5 +180,33 @@ export const logout = async (req, res) => {
 
 
 
+  import nodemailer from "nodemailer";
 
+
+  export async function sendEmail(to, subject, text) {
+      const transporter = nodemailer.createTransport({
+       host: 'smtp.gmail.com',
+       port: 587,
+       secure: false,
+        auth: {
+          user: 'teamintellidoc@gmail.com', // your email
+          pass: 'kkpr aeiv slyu tlya' // your email password
+        }
+      });
+    
+      const mailOptions = {
+        from: 'teamintellidoc@gmail.com', // your email
+        to: to,
+        subject: subject,
+        html: text
+      };
+       
+      try{
+        await transporter.sendMail(mailOptions);
+      } catch(err){
+        const error = new HttpError('Internal server error',500);
+        return next(error);
+      }
+      
+    }
   
